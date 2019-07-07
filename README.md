6e50c5ec26fb9b3ea3d55612c13cc03790455016
kaide <kidnohr2000@yahoo.co.jp>
2019-07-07 14:10:38 +0900

    add runserver mode (dev)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ml_webapi
+# 開発環境
+
+http://localhost:13081/api/iris?sepal_length=5.1&sepal_width=1&petal_length=1&petal_width=1
--- server/server/wsgi_prd.py
+++ server/server/wsgi_prd.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apps.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+
+pth = os.path.join(os.path.dirname(__file__), '..')
+if pth not in sys.path:
+    sys.path.insert(1, pth)
+del pth
 
 application = get_wsgi_application()
